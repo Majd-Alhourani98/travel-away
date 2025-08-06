bba602f4bdc704efaa4bc5ea0e6c2f7da3737fdf
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-08-06 16:14:12 +0300

    Building the Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <h1>Hello World</h1>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}
