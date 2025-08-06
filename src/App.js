604abd6c483465abedf11163b31b1c9169106e95
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-08-06 16:50:52 +0300

    rendering list of Item component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 3, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -12,6 +17,7 @@ export default function App() {
 function Logo() {
   return <h1>Far Away</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -19,8 +25,29 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+
+      <button>X</button>
+    </li>
+  );
 }
 
 function Stats() {
