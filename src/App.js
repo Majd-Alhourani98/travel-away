170541f9c3b5af6a12ed111a62be5010eaff2524
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-08-07 16:13:37 +0300

    Added dynamic stats footer showing total, packed, and packing completion percentage

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -100,7 +100,6 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 }
 
 function Item({ item, onDeleteItem, onToggleItem }) {
-  const [packed, setPacked] = useState(false);
   return (
     <li>
       <input
@@ -117,10 +116,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="footer">
+        <em>Start adding some itmes to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.floor((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (x%)</em>
+      <em>
+        {percentage === 100
+          ? "You got eveything! Ready to go"
+          : `You have ${numItems} items on your list, and you already packed{" "}
+        ${numPacked} (${percentage} %)`}
+      </em>
     </footer>
   );
 }
